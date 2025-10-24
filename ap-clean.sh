5c54da755e3fd7af53e9cbca88caa13223283755
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-10-24 15:34:18 -0400

    Fix JSON parsing logic

--- ap-clean.sh
+++ ap-clean.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+set -x
+
 if [[ -n $AP_PLAYER && $AP_ROOM =~ ^(https?://archipelago.gg/room/)?([^/]+)$ ]]; then
     curl "${AP_ROOM_BASE:-https://archipelago.gg}/api/room_status/${BASH_REMATCH[2]}" -o /tmp/room
     mkdir /factorio/mods
     cd /factorio/mods
     if [[ -z $AP_NO_MANAGE_MOD ]]; then
         rm AP-*.zip
-        curl -OJ "${AP_ROOM_BASE:-https://archipelago.gg}$(jq -r '.downloads[.players | map(.[0] == "'"$AP_PLAYER"'") | index(true)].download' /tmp/room)"
+        SLOT="$(jq -r '.players | map(.[0] == "'"$AP_PLAYER"'") | index(true) + 1' /tmp/room)"
+        curl -OJ "${AP_ROOM_BASE:-https://archipelago.gg}$(jq -r ".downloads[] | select(.slot == $SLOT) | .download" /tmp/room)"
     fi
     AP_URL="${AP_PROTOCOL:-wss}://$AP_PLAYER:$AP_PASSWORD@${AP_URL_BASE:-archipelago.gg:}$(jq -r '.last_port' /tmp/room)";
 fi
